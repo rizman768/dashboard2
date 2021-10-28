79fca3f2aadc431a8bbf45d385e39c8c9988ab3b
rizman768 <rizqimaulana768@gmail.com>
2021-10-28 10:51:36 +0700

    ubah title

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Viana</a>
+            <a class="navbar-brand ps-3" href="index.html">KAI VIANA</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
